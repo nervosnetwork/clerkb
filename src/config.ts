6e63774f969a948db58f1f112a9633469205d916
Xuejie Xiao <xxuejie@gmail.com>
2020-11-18 10:28:13 +0800

    PoASetup serialization into on-chain cell data

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,16 @@
 import Ajv from "ajv";
+import { Reader } from "ckb-js-toolkit";
 import { readFileSync } from "fs";
 import schema from "./config_schema.json";
 
-export function readConfig(filename: string) {
+// TODO: build a proper type later
+export type Config = any;
+
+export function readConfig(filename: string): Config {
   return parseConfig(readFileSync("test.json", "utf8"));
 }
 
-export function parseConfig(configData: string) {
+export function parseConfig(configData: string): Config {
   const config = JSON.parse(configData);
   const ajv = new Ajv();
   const validate = ajv.compile(schema);
@@ -37,4 +41,38 @@ export function parseConfig(configData: string) {
     throw new Error("Invalid change threshold!");
   }
   return config;
+}
+
+export function configToPoASetup(config: Config): ArrayBuffer {
+  const poa_setup = config.poa_setup;
+  const length =
+    44 +
+    poa_setup.identities.length * new Reader(poa_setup.identities[0]).length();
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  const uint8array = new Uint8Array(buffer);
+  uint8array.set(
+    new Uint8Array(new Reader(poa_setup.code_hash).toArrayBuffer()),
+    0
+  );
+  let flag = 0;
+  if (poa_setup.hash_type === "type") {
+    flag |= 1;
+  }
+  if (poa_setup.interval_uses_seconds) {
+    flag |= 2;
+  }
+  view.setUint8(32, flag);
+  view.setUint8(33, poa_setup.identity_size);
+  view.setUint8(34, poa_setup.identities.length);
+  view.setUint8(35, poa_setup.aggregator_change_threshold);
+  view.setUint32(36, poa_setup.subblock_intervals, true);
+  view.setUint32(40, poa_setup.subblocks_per_interval, true);
+  for (let i = 0; i < poa_setup.identities.length; i++) {
+    uint8array.set(
+      new Uint8Array(new Reader(poa_setup.identities[i]).toArrayBuffer()),
+      44 + i * poa_setup.identity_size
+    );
+  }
+  return buffer;
 }
